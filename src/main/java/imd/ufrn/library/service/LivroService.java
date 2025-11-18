6a7546da8950edfe1bbce677d7644ac856060fca
Gabrielly Freire <gabylopesfreire@gmail.com>
2025-11-18 20:33:48 -0300

    feat: implementa o cadastro de livro e ajustes nos testes correspondentes

--- src/main/java/imd/ufrn/library/service/LivroService.java
+++ src/main/java/imd/ufrn/library/service/LivroService.java
@@ -3,6 +3,7 @@ package imd.ufrn.library.service;
 import imd.ufrn.library.model.Livro;
 import imd.ufrn.library.repository.LivroRepository;
 
+import jakarta.validation.ValidationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,18 @@ public class LivroService {
     }
 
     public Livro cadastrarLivro(Livro livro) {
-        return null;
+        if (livro.getTitulo() == null || livro.getTitulo().trim().isEmpty()) {
+            throw new ValidationException("O título do livro não pode ser vazio.");
+        }
+
+        if(livro.getAutor() == null || livro.getAutor().trim().isEmpty()) {
+            throw new ValidationException("O autor do livro não pode ser vazio.");
+        }
+
+        if (livro.getQuantidadeTotal() == null || livro.getQuantidadeTotal() <= 0) {
+            throw new ValidationException("A quantidade total de livros deve ser maior que zero.");
+        }
+
+        return livroRepository.save(livro);
     }
 }
--- src/test/java/imd/ufrn/library/LivroServiceTest.java
+++ src/test/java/imd/ufrn/library/LivroServiceTest.java
@@ -43,11 +43,11 @@ public class LivroServiceTest {
 
         when(livroRepository.save(LivroData.livroComQuantidadeTotalIgualAUm)).thenReturn(LivroData.livroComQuantidadeTotalIgualAUm);
 
-        Livro resultado = livroService.cadastrarLivro(LivroData.livroValido);
+        Livro resultado = livroService.cadastrarLivro(LivroData.livroComQuantidadeTotalIgualAUm);
 
-        assertEquals(LivroData.livroValido, resultado);
-        assertEquals(LivroData.livroValido.getQuantidadeTotal(), resultado.getQuantidadeTotal());
-        assertEquals(LivroData.livroValido.getQuantidadeTotal(), resultado.getQuantidadeDisponivel());
+        assertEquals(LivroData.livroComQuantidadeTotalIgualAUm, resultado);
+        assertEquals(LivroData.livroComQuantidadeTotalIgualAUm.getQuantidadeTotal(), resultado.getQuantidadeTotal());
+        assertEquals(LivroData.livroComQuantidadeTotalIgualAUm.getQuantidadeTotal(), resultado.getQuantidadeDisponivel());
     }
 
     @Test
@@ -79,7 +79,7 @@ public class LivroServiceTest {
             livroService.cadastrarLivro(LivroData.livroComQuantidadeNegativa);
         });
 
-        assertEquals("A quantidade total de livros não pode ser negativa.", ex.getMessage());
+        assertEquals("A quantidade total de livros deve ser maior que zero.", ex.getMessage());
     }
 
     @Test
